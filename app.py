b32466660a01d9406750b7532ffed1f4afc84b3d
rasturies <70368117+rasturies@users.noreply.github.com>
2024-02-15 20:27:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola Mundo!'
+    print("Solicitud recibida en la ruta /")
+    return 'Hola Mundo8888!'
 
 if __name__ == '__main__':
+    print("Iniciando la aplicación en el puerto 80...")
     app.run(host='0.0.0.0', port=80)
 
